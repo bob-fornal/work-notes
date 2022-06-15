3d6ec83d7785ed2e6c9458903156ff6fd28dade4
Bob Fornal <bob@fornal.org>
2022-06-15 15:17:31 -0400

    Input Filters

---
+++ Front-End/RxJS/patterns/pattern--005-input-filters.js
@@ -0,0 +1,52 @@
+
+// <input type="text" (input)="onCategoryFilterChange($event.target.value)" />
+// <input type="text" (input)="onCountryFilterChange($event.target.value)" />
+
+import { BehaviorSubject, combineLatest } from "rxjs";
+
+export class FilterContent {
+
+  categoryFilter$ = new BehaviorSubject('');
+  countryFilter$ = new BehaviorSubject('');
+
+  products$ = new BehaviorSubject([
+    { title: 'First', category: 'Square', country: 'USA' },
+    { title: 'Second', category: 'Circle', country: 'USA' },
+    { title: 'Third', category: 'Square', country: 'DE' },
+    { title: 'Fourth', category: 'Square', country: 'DE' },
+    { title: 'Fifth', category: 'Oval', country: 'ENG' },
+    { title: 'Sixth', category: 'Circle', country: 'ENG' }
+  ]);
+  filtered = [];
+
+  constructor() {
+    this.init();
+    console.log(this.filtered);
+  }
+
+  init = () => {
+    combineLatest([
+      this.products$,
+      this.categoryFilter$,
+      this.countryFilter$
+    ]).subscribe(([ products, selectedCategory, selectedCountry ]) => {
+      this.filtered = products.filter((product) => {
+        return product.category.includes(selectedCategory) && product.country.includes(selectedCountry);
+      });
+      console.log(this.filtered);
+    });
+  };
+
+  onCategoryFilterChange = (category) => {
+    this.categoryFilter$.next(category);
+  };
+
+  onCountryFilterChange = (country) => {
+    this.countryFilter$.next(country);
+  };
+
+}
+
+const content = new FilterContent();
+content.onCategoryFilterChange('Square');
+content.onCountryFilterChange('DE');
