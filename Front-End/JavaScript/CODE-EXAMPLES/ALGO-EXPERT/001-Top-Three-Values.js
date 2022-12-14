07ae64cf331727e7db636491f79da9e90b94fdea
Bob Fornal <bob.fornal@leadingedje.com>
2022-12-13 22:41:33 -0500

    Update 001-Top-Three-Values.js

--- Front-End/JavaScript/CODE-EXAMPLES/ALGO-EXPERT/001-Top-Three-Values.js
+++ Front-End/JavaScript/CODE-EXAMPLES/ALGO-EXPERT/001-Top-Three-Values.js
@@ -1,29 +1,24 @@
 
 /*
-  Finding the top three values from an array without sorting.
-  Receives three integers minimum, no duplicate values.
-  Returns an array of three integers.
+  FIND THREE LARGEST NUMBERS
+  Write a function that takes an array of at least three integers and, without
+  sorting the input array, returns a sorted array of the three largest integers
+  in the input array.
+
+  The function should return duplicate integers if necessary.
 */
 
 function simpleTopThree(check) {
-  const checkNoDuplicates = [...new Set(check)];
-  const sorted = checkNoDuplicates.sort((a, b) => b - a);
+  // const checkNoDuplicates = [...new Set(check)];
+  const sorted = check.sort((a, b) => b - a);
   sorted.length = 3;
   return sorted;
-}
-
-function findTopThree2(check) {
-  let checkNoDuplicates = [...new Set(check)].sort((a, b) => b - a);
-
-  checkNoDuplicates.length = 3;
-  return checkNoDuplicates;
 }
 
 function findTopThree1(check) {
   let result = [];
 
-  let checkNoDuplicates = [...new Set(check)];
-  checkNoDuplicates.forEach((num) => {
+  check.forEach((num) => {
     if (result.length === 0) {
       result.push(num);
     } else {
@@ -45,49 +40,53 @@ function findTopThree1(check) {
   return result;
 }
 
-function findTopThree3 (check) {
-  let checkNoDuplicates = [...new Set(check)];
+function findTopThree2 (check) {
+  const top1 = Math.max(...check);
+  let index = check.indexOf(top1);
+  check.splice(index, 1);
 
-  const top1 = Math.max(...checkNoDuplicates);
-  let index = checkNoDuplicates.indexOf(top1);
-  checkNoDuplicates.splice(index, 1);
+  const top2 = Math.max(...check);
+  index = check.indexOf(top2);
+  check.splice(index, 1);
 
-  const top2 = Math.max(...checkNoDuplicates);
-  index = checkNoDuplicates.indexOf(top1);
-  checkNoDuplicates.splice(index, 1);
-
-  const top3 = Math.max(...checkNoDuplicates);
+  const top3 = Math.max(...check);
   
   return [top1, top2, top3];
 }
 
-
-// console.log(simpleTopThree([1, 2, 3, -5, 122, 2000, 3]));
-// console.log(findTopThree1([1, 2, 3, -5, 122, 2000, 3]));
-// console.log(findTopThree3([1, 2, 3, -5, 122, 2000, 3]));
-// console.log(findTopThree1([1, 2, 3, 3, 4, 4, 5, 6]));
-// console.log(findTopThree1([6, 5, 4, 3, 2, 1]));
+function testFunctionality() {
+  console.log('simple', simpleTopThree([1, 2, 3, -5, 122, 2000, 3]));
+  console.log('three-1', findTopThree1([1, 2, 3, -5, 122, 2000, 3]));
+  console.log('three-2', findTopThree2([1, 2, 3, -5, 122, 2000, 3]));
+  console.log('three-1', findTopThree1([1, 2, 3, 3, 4, 4, 5, 6]));
+  console.log('three-1', findTopThree1([6, 5, 4, 3, 2, 1]));
+}
+// testFunctionality();
 
 const randomArray = (length, max) => [...new Array(length)]
     .map(() => Math.round(Math.random() * max));
 
-const checkArray = randomArray(100, 10000);
-console.log(checkArray);
-const iterations = 1_000_000;
-console.time('simpleTopThree');
-for (let i = 0, len = iterations; i < len; i++) {
-  simpleTopThree(checkArray);
-}
-console.timeEnd('simpleTopThree');
+const checkArray = randomArray(10_000_000, 10000);
 
-console.time('findTopThree1');
-for (let i = 0, len = iterations; i < len; i++) {
-  findTopThree1(checkArray);
+function testTiming() {
+  console.log(checkArray);
+  const iterations = 1_000_000;
+  console.time('simpleTopThree');
+  for (let i = 0, len = iterations; i < len; i++) {
+    simpleTopThree(checkArray);
+  }
+  console.timeEnd('simpleTopThree');
+  
+  console.time('findTopThree1');
+  for (let i = 0, len = iterations; i < len; i++) {
+    findTopThree1(checkArray);
+  }
+  console.timeEnd('findTopThree1');
+  
+  console.time('findTopThree2');
+  for (let i = 0, len = iterations; i < len; i++) {
+    findTopThree2(checkArray);
+  }
+  console.timeEnd('findTopThree2');  
 }
-console.timeEnd('findTopThree1');
-
-console.time('findTopThree3');
-for (let i = 0, len = iterations; i < len; i++) {
-  findTopThree3(checkArray);
-}
-console.timeEnd('findTopThree3');
+testTiming();
