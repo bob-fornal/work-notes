09441cb696f91228229aa1d9c263034dcd413c89
Bob Fornal <bob.fornal@leadingedje.com>
2023-01-28 21:01:30 -0500

    Update 12--Map-versus-forEach.js

--- Front-End/JavaScript/CODE-EXAMPLES/INTERVIEW-QUESTIONS/12--Map-versus-forEach.js
+++ Front-End/JavaScript/CODE-EXAMPLES/INTERVIEW-QUESTIONS/12--Map-versus-forEach.js
@@ -6,7 +6,7 @@ const new1 = test1Array.map((value) => value * 2);
 
 const test2Array = [1, 2, 3, 4, 5];
 const new2 = test2Array.forEach((value) => value * 2);
-// console.log(test2Array, new2);
+// console.log(test2Array, new2, test2Array);
 
 const test3Array = [1, 2, 3, 4, 5];
 const new3 = test2Array.forEach((value, index) => {
@@ -16,9 +16,13 @@ const new3 = test2Array.forEach((value, index) => {
 
 let test4 = [];
 let test5 = [];
+let test6 = [];
+let test7 = [];
 for (let i = 0, len = 10_000_000; i < len; i++) {
   test4.push(i);
   test5.push(i);
+  test6.push(i);
+  test7.push(i);
 }
 
 console.time('map test');
@@ -26,6 +30,19 @@ const new4 = test4.map((value) => value * 2);
 console.timeEnd('map test');
 
 console.time('forEach test');
-test5.forEach((value) => value * 2);
+test5.forEach((value, index) => {
+  test5[index] = value * 2;
+});
 console.timeEnd('forEach test');
 
+console.time('for-loop no-size test');
+for (let i = 0; i < test6.length; i++) {
+  test6[i] = test6[i] * 2;
+}
+console.timeEnd('for-loop no-size test');
+
+console.time('for-loop size test');
+for (let i = 0, len = test7.length; i < len; i++) {
+  test7[i] = test7[i] * 2;
+}
+console.timeEnd('for-loop size test');
